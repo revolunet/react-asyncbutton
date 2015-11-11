5d57ef520e9e8e512c1d8d0ce81888b9728634e2
Julien Bouquillon <julien@bouquillon.com>
2015-11-11 15:59:16 +0100

    initial commit

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import ReactAsyncbutton from '../../lib/index';
+import AsyncButton from '../../lib/index';
 
 class App extends Component {
-
-  click() {
-    alert('Roger that !');
+  constructor(...props) {
+    super(...props);
+    this.onSuccess = ::this.onSuccess;
   }
-
+  onSuccess(res) {
+    // for example purpose
+    this.refs.log.innerHTML = 'Response 200 received';
+  }
   render() {
+    let url = 'https://api.github.com/users/octocat/repos';
     return (
       <div className='example'>
         <h1>react-asyncbutton</h1>
-        <ReactAsyncbutton click={ this.click } name='Click me'/>
+        <AsyncButton url={ url } onSuccess={ this.onSuccess }/>
+        <div ref='log' style={ {marginTop: 20 } }></div>
       </div>
     );
   }
